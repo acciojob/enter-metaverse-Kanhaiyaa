36ca3d8b3b534b500835913f43431249e4554600
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-20 17:20:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function enterMetaverse(){
+
+        var enter=document.getElementById("status")
+        
+         enter.textContent="Enter Metaverse"
+
+         var h1Element=document.createElement('h1')
+         h1Element.textContent='Enter Metaverse'
+         document.body.appendChild(h1Element)
+
+     }
